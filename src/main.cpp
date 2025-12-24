d1fad2070b36e959b90dc608d8ffb9cd54e547f6
Aliaksandr Khramau <axel.khr@gmail.com>
2025-12-24 20:54:50 +0300

    init: init basic project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow/mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QApplication::setOrganizationName("Pramen");
+    QApplication::setApplicationName("SAD");
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
